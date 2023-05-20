e062f5eef81a0267daeab1a4279e966d3c5b38b7
Hamza <muhammadhamzaimam786@gmail.com>
2023-05-20 16:21:11 +0100

    Deleted unnecessary files and started ButtonWithIcon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { ButtonWithIcon } from './components/ButtonWithIcon'
 
 function App() {
   return (
     <div className="App">
+    <ButtonWithIcon/>
       <p> Starting new project </p>
     </div>
   );
---
+++ client/src/components/ButtonWithIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ButtonWithIcon: React.FC = () => {
+    return(
+        <button>
+            Testing new component
+        </button>
+    )
+}
