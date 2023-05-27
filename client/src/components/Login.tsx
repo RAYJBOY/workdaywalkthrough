0a2416c8c5559dda81942c39a3ec0836c31a2d06
Abdalraof Benaesha <77442379+Abenaesha@users.noreply.github.com>
2023-05-27 17:46:04 +0100

    6 - create sign in component
    * Create a LoginModal - #6
    
    * Renaming component - #6

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Dialog, DialogTitle, DialogContent, Button, TextField, Box } from "@mui/material";
+
+type LoginModalProps = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+const LoginModal: React.FC<LoginModalProps> = ({ open, handleClose }) => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const switchMode = () => {
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>{isSignUp ? "Sign Up" : "Sign In"}</DialogTitle>
+      <DialogContent>
+        <TextField
+          required
+          label="Username"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        {isSignUp && (
+          <TextField
+          required type="email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        )}
+        <TextField
+          required type="password"
+          label="Password"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        {isSignUp && (
+          <TextField 
+            required type="password"
+            label="Confirm Password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+        )}
+        <Box sx={{ mt: 2 }}>
+          <Button variant="contained" color="primary" fullWidth>
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </Button>
+          {!isSignUp && (
+            <Button
+              color="secondary"
+              fullWidth
+              onClick={() => console.log("Reset Password Clicked")}
+            >
+                Reset Password
+            </Button>
+          )}
+        </Box>
+        <Box sx={{ mt: 2 }}>
+          <Button onClick={switchMode} fullWidth>
+            {isSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
+          </Button>
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default LoginModal;
