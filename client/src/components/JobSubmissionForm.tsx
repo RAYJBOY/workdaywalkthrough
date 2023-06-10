8ddd20e1b7c331068faa9c787cabb64a328eebda
Hamza <muhammadhamzaimam786@gmail.com>
2023-06-10 17:44:57 +0100

    polished job details form

--- client/src/components/JobSubmissionForm.tsx
+++ client/src/components/JobSubmissionForm.tsx
@@ -20,68 +20,66 @@ const jobTypes = [
 
 export const JobSubmissionForm = () => {
   return (
-    <div style={{'marginTop':'10px', 'display':'grid', 'backgroundColor': 'grey'}}>
+    <div style={{'marginTop':'10px', 
+                 'display':'grid', 
+                 'gridTemplateColumns': '1fr 1fr 1fr',
+                 'gap': '5%'
+                }}>
 
-        <div style={{'display': 'flex', 'justifyContent':'center'}}>
+        <div style={{'display': 'flex', 
+                     'justifyContent':'center', 
+                     'gridColumn': 'span 3'
+                    }}>
             <Typography variant="h3">Job Details</Typography>
         </div>
 
-        <div style={{'display': 'flex', 'flexDirection': 'row', 'justifyContent': 'space-evenly', 'backgroundColor':'green'}}>
-            <TextField 
-                required 
-                id='outline-required' 
-                label='Job Title'
-                placeholder="Enter job title"
-                margin="normal"
-                style={{'width':'20%'}}
+        <TextField 
+            required 
+            id='outline-required' 
+            label='Job Title'
+            placeholder="Enter job title"
+            margin="normal"
+        />
+
+        <TextField 
+            required 
+            id='outline-required' 
+            label='Job type'
+            placeholder="Select job type"
+            margin="normal"
+            select
+        >
+            {jobTypes.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                </MenuItem>
+            ))}
+        </TextField>
+
+        <TextField 
+            required 
+            id='outline-required' 
+            label='Location'
+            placeholder="Enter location"
+            margin="normal"
+        />
+
+        <TextField 
+            required 
+            label='Description'
+            placeholder="Job Description"
+            margin="normal"
+            multiline
+            style={{'gridColumn': 'span 3'}}
+        />
+
+        <FormControl margin="normal">
+            <InputLabel>Amount</InputLabel>
+            <OutlinedInput
+                startAdornment={<InputAdornment position="start">£</InputAdornment>}
+                label="Amount"
             />
-
-            <TextField 
-                required 
-                id='outline-required' 
-                label='Job type'
-                placeholder="Select job type"
-                margin="normal"
-                select
-                style={{'width':'20%'}}
-            >
-                {jobTypes.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                        {option.value}
-                    </MenuItem>
-                ))}
-            </TextField>
-
-            <TextField 
-                required 
-                id='outline-required' 
-                label='Location'
-                placeholder="Enter location"
-                margin="normal"
-                style={{'width':'20%'}}
-            />
-
-            <TextField 
-                required 
-                id='outline-required' 
-                label='Annual Salary'
-                placeholder="Enter salary"
-                margin="normal"
-                style={{'width':'20%'}}
-            />
-        </div>
-
-        <div>
-        <FormControl>
-                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-                <OutlinedInput
-                    startAdornment={<InputAdornment position="start">£</InputAdornment>}
-                    label="Amount"
-                />
-            </FormControl>
-        </div>
-
-        
+        </FormControl>        
     </div>
   )
 };
