02458de7269440c721c9355ac30876fd890cc1a0
Hamza <muhammadhamzaimam786@gmail.com>
2023-07-18 20:16:14 +0100

    started meet the team on home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,23 +1,29 @@
 import { NavBar } from "../components/NavBar";
 import { homePageNavBar } from "../constants/NavBarConstants";
 import { Typography } from "@mui/material";
-import '../styles/homePage.css'
-
+import "../styles/homePage.css";
 
 export const HomePage = () => {
   return (
     <>
-      <NavBar links={homePageNavBar} userIsLoggedIn={false}/>
+      <NavBar links={homePageNavBar} userIsLoggedIn={false} />
       <div className="mainDisplayContainer">
         <Typography variant="h2" gutterBottom>
           Our Vision
         </Typography>
         <div className="mainDisplay">
           <Typography variant="h5" gutterBottom align="center">
-            Our objective is to help increase transparency between candidates looking for a role in the tech industry and their future potential employers.
+            To enhance transparency in recruitment where candidates have a
+            better understanding of the role they're applying to. Having a good
+            idea of what the role entails will provide for better suited
+            candidates and will ultimately benefit everyone.
           </Typography>
         </div>
+
+        <Typography variant="h2" gutterBottom sx={{marginTop: '5%'}}>
+          Meet The Team
+        </Typography>
       </div>
     </>
-  )
+  );
 };
