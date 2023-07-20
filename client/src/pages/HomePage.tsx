fcc33aba564b5bf72439f19f3be5f57efd8b20ea
Hamza <muhammadhamzaimam786@gmail.com>
2023-07-20 20:50:09 +0100

    finished meet the team section, need pictures

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 import { NavBar } from "../components/NavBar";
 import { homePageNavBar } from "../constants/NavBarConstants";
-import { Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 import "../styles/homePage.css";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export const HomePage = () => {
   return (
@@ -23,6 +25,66 @@ export const HomePage = () => {
         <Typography variant="h2" gutterBottom sx={{marginTop: '5%'}}>
           Meet The Team
         </Typography>
+        <div className="meetTheTeamContainer">
+
+          <div style={{width:'320px', height:'212px'}}>
+            <img src={require('../public/Hamza.jpg')} alt="Hamza" style={{width:'100%', height:'100%', borderRadius:'5%'}}/>
+          </div>
+          <div className="meetTheTeamText">
+            <Typography variant="h5" gutterBottom>
+              Muhammad Hamza Imam
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              Full Stack Developer
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              Reading, United Kingdom
+            </Typography>
+            <Typography variant="h5" sx={{position:'absolute', bottom:'0', width:'100%'}}>
+              <a href="https://www.linkedin.com/in/muhammad-hamza-imam-402128196" target="_blank" rel="noreferrer" style={{color:'inherit', paddingRight:'5%'}}><LinkedInIcon fontSize="large"/></a>
+              <a href="https://github.com/RAYJBOY" target="_blank" rel="noreferrer" style={{color:'inherit'}}><GitHubIcon fontSize="large"/></a>
+            </Typography>
+          </div>
+
+          <div style={{width:'320px', height:'212px'}}>
+            <img src={require('../public/Hamza.jpg')} alt="Hamza" style={{width:'100%', height:'100%', borderRadius:'5%'}}/>
+          </div>
+          <div className="meetTheTeamText">
+            <Typography variant="h5" gutterBottom>
+              Abdalraof Benaesha
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              Software Engineer
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              Manchester, United Kingdom
+            </Typography>
+            <Typography variant="h5" sx={{position:'absolute', bottom:'0', width:'100%'}}>
+              <a href="https://www.linkedin.com/in/abdalraof-benaesha-636231aa/" target="_blank" rel="noreferrer" style={{color:'inherit', paddingRight:'5%'}}><LinkedInIcon fontSize="large"/></a>
+              <a href="https://github.com/Abenaesha" target="_blank" rel="noreferrer" style={{color:'inherit'}}><GitHubIcon fontSize="large"/></a>           
+            </Typography>
+          </div>
+
+          <div style={{width:'320px', height:'212px'}}>
+            <img src={require('../public/Hamza.jpg')} alt="Hamza" style={{width:'100%', height:'100%', borderRadius:'5%'}}/>
+          </div>
+          <div className="meetTheTeamText">
+            <Typography variant="h5" gutterBottom>
+              Qasim Kamran the SoyLord
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              Software Engineer
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              Manchester, United Kingdom
+            </Typography>
+            <Typography variant="h5" sx={{position:'absolute', bottom:'0', width:'100%'}}>
+              <a href="https://www.linkedin.com/in/qasim-kamran-89b731198/" target="_blank" rel="noreferrer" style={{color:'inherit', paddingRight:'5%'}}><LinkedInIcon fontSize="large"/></a>
+              <a href="https://github.com/qasimkamran" target="_blank" rel="noreferrer" style={{color:'inherit'}}><GitHubIcon fontSize="large"/></a>           
+            </Typography>
+          </div>
+
+        </div>
       </div>
     </>
   );
