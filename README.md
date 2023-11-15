2bf7c51bd3576ed244d6d60519f507e6ab91ad53
Hamza <muhammadhamzaimam786@gmail.com>
2023-11-15 20:20:42 +0000

    added instructions to run server in watch mode

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # workdaywalkthrough
 A website that gives users more information about the roles they are applying to
+
+To run the node server in watch mode, open VSCode from WSL using "code .". In your VSCode, open 2 terminals in ./server.
+In the first, run "nodemon -L ./dist", and in the other run "npx tsc -w".
--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,9 @@ import express, {Express, Request, Response} from 'express'
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello from express 45.0')
+    res.send('hello from express 486.0')
 });
 
-app.listen(8000, () => {
-    console.log('Now listening on port 8000')
+app.listen(8080, () => {
+    console.log('Now listening on port 8080')
 })
