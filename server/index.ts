7b02ca2387036066f858a6983bb2db7fdb383d06
Hamza <muhammadhamzaimam786@gmail.com>
2023-12-03 15:52:17 +0000

    data can be inserted into mysql workbench db

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,22 @@
 import express, {Express, Request, Response} from 'express'
+const mysql2 = require('mysql2');
 
 const app: Express = express();
 
+const db = mysql2.createConnection({
+    user: 'root',
+    host: 'winhost',
+    password: 'root',
+    database: 'workdaywalkthrough'
+})
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello from express 486.0')
+    db.query('INSERT INTO users (username, password, isEmployer) VALUES ("userTwo", "passwordTwo", 1)', (error: any, result: any) => {
+        if(error) {
+            console.log('Error putting values into db: ', error)
+        }
+        res.send(result);
+    })
 });
 
 app.listen(8080, () => {
